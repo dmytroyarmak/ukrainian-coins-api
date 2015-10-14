ebd8ba0cfa77f994b50ee943a39477ffbce07685
Dmytro Yarmak <dmytroy@backbase.com>
2015-10-14 20:17:00 +0300

    Implement getDetails

--- test/ukrainian-coins-api.js
+++ test/ukrainian-coins-api.js
@@ -53,8 +53,7 @@ describe('ukrainianCoinsApi', function() {
     });
   });
 
-
-  describe.only('#getList()', function () {
+  describe('#getList()', function () {
     it('should be a function', function() {
       expect(ukrainianCoinsApi.getList).to.be.a('function');
     });
@@ -85,6 +84,7 @@ describe('ukrainianCoinsApi', function() {
 
         it('should contain objects with id, name, metal, denomination and date', function() {
           resolvedData.forEach(function(item) {
+
             expect(item).to.be.an('object');
 
             expect(item).to.have.property('id').that.is.a('number');
@@ -93,6 +93,59 @@ describe('ukrainianCoinsApi', function() {
             expect(item).to.have.property('denomination').that.is.a('string');
             expect(item).to.have.property('date').that.is.a('string');
           });
+        });
+      });
+    });
+  });
+
+  describe('#getDetails(id)', function () {
+    it('should be a function', function() {
+      expect(ukrainianCoinsApi.getDetails).to.be.a('function');
+    });
+
+    describe('when called', function() {
+      var result;
+
+      beforeEach(function() {
+        result = ukrainianCoinsApi.getDetails(1);
+      });
+
+      it('should return a promise', function() {
+        expect(result).to.be.an.instanceOf(Promise);
+      });
+
+      describe('resolved data', function() {
+        var resolvedData;
+
+        beforeEach(function() {
+          return result.then(function(data) {
+            resolvedData = data;
+          });
+        });
+
+        it('should be an object', function() {
+          expect(resolvedData).to.be.an('object');
+        });
+
+        it('should contain all properties', function() {
+          expect(resolvedData).to.have.property('id').that.is.a('number');
+          expect(resolvedData).to.have.property('name').that.is.a('string');
+          expect(resolvedData).to.have.property('metal').that.is.a('string');
+          expect(resolvedData).to.have.property('denomination').that.is.a('string');
+          expect(resolvedData).to.have.property('date').that.is.a('string');
+          expect(resolvedData).to.have.property('weight').that.is.a('number');
+          expect(resolvedData).to.have.property('diameter').that.is.a('number');
+          expect(resolvedData).to.have.property('thickness').that.is.a('number');
+          expect(resolvedData).to.have.property('quality').that.is.a('string');
+          expect(resolvedData).to.have.property('edge').that.is.a('string');
+          expect(resolvedData).to.have.property('mintage').that.is.a('number');
+          expect(resolvedData).to.have.property('categoryId').that.is.a('number');
+          expect(resolvedData).to.have.property('category').that.is.a('string');
+          expect(resolvedData).to.have.property('generalDescription').that.is.a('string');
+          expect(resolvedData).to.have.property('averseDescription').that.is.a('string');
+          expect(resolvedData).to.have.property('reverseDescription').that.is.a('string');
+          expect(resolvedData).to.have.property('averseImgUrl').that.is.a('string');
+          expect(resolvedData).to.have.property('reverseImgUrl').that.is.a('string');
         });
       });
     });
