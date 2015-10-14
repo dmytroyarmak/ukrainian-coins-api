7347807d52d6a45b1579f1c84f693ea5cf1bd516
Dmytro Yarmak <dmytroyarmak@gmail.com>
2015-10-14 17:05:38 +0300

    Add tests for getList method

--- test/ukrainian-coins-api.js
+++ test/ukrainian-coins-api.js
@@ -39,17 +39,59 @@ describe('ukrainianCoinsApi', function() {
           resolvedData.forEach(function(item) {
             expect(item).to.be.an('object');
 
-            expect(item).to.have.property('id')
-              .that.is.a('number');
-
-            expect(item).to.have.property('name')
-              .that.is.a('string');
+            expect(item).to.have.property('id').that.is.a('number');
+            expect(item).to.have.property('name').that.is.a('string');
           });
         });
 
         it('should contain objects only with positive id', function() {
           resolvedData.forEach(function(item) {
             expect(item.id).to.be.above(0);
+          });
+        });
+      });
+    });
+  });
+
+
+  describe.only('#getList()', function () {
+    it('should be a function', function() {
+      expect(ukrainianCoinsApi.getList).to.be.a('function');
+    });
+
+    describe('when called', function() {
+      var result;
+
+      beforeEach(function() {
+        result = ukrainianCoinsApi.getList();
+      });
+
+      it('should return a promise', function() {
+        expect(result).to.be.an.instanceOf(Promise);
+      });
+
+      describe('resolved data', function() {
+        var resolvedData;
+
+        beforeEach(function() {
+          return result.then(function(data) {
+            resolvedData = data;
+          });
+        });
+
+        it('should be an array', function() {
+          expect(resolvedData).to.be.an.instanceOf(Array);
+        });
+
+        it('should contain objects with id, name, metal, denomination and date', function() {
+          resolvedData.forEach(function(item) {
+            expect(item).to.be.an('object');
+
+            expect(item).to.have.property('id').that.is.a('number');
+            expect(item).to.have.property('name').that.is.a('string');
+            expect(item).to.have.property('metal').that.is.a('string');
+            expect(item).to.have.property('denomination').that.is.a('string');
+            expect(item).to.have.property('date').that.is.a('string');
           });
         });
       });
