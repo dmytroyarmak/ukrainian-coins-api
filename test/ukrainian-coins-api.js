cbfb96f0004bf586c6e26a71b73402e515dac725
Dmytro Yarmak <dmytroyarmak@gmail.com>
2015-10-14 16:42:28 +0300

    Implement getСategories method

--- test/ukrainian-coins-api.js
+++ test/ukrainian-coins-api.js
@@ -10,5 +10,49 @@ describe('ukrainianCoinsApi', function() {
     it('should be a function', function() {
       expect(ukrainianCoinsApi.getСategoriess).to.be.a('function');
     });
+
+    describe('when called', function() {
+      var result;
+
+      beforeEach(function() {
+        result = ukrainianCoinsApi.getСategoriess();
+      });
+
+      it('should return a promise', function() {
+        expect(result).to.be.an.instanceOf(Promise);
+      });
+
+      describe('resolved data', function() {
+        var resolvedData;
+
+        beforeEach(function() {
+          return result.then(function(data) {
+            resolvedData = data;
+          });
+        });
+
+        it('should be an array', function() {
+          expect(resolvedData).to.be.an.instanceOf(Array);
+        });
+
+        it('should contain objects with id and name', function() {
+          resolvedData.forEach(function(item) {
+            expect(item).to.be.an('object');
+
+            expect(item).to.have.property('id')
+              .that.is.a('number');
+
+            expect(item).to.have.property('name')
+              .that.is.a('string');
+          });
+        });
+
+        it('should contain objects only with positive id', function() {
+          resolvedData.forEach(function(item) {
+            expect(item.id).to.be.above(0);
+          });
+        });
+      });
+    });
   });
 });
